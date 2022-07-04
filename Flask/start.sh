2a86fcb2c1dfe18719b04443d62b9a9da44f45f0
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-07-04 07:14:08 -0300

    Correcao docker build

--- Flask/start.sh
+++ Flask/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="flask_prediction"
-docker build --platform linux/x86_64 -t ${app} .
-docker run --platform linux/x86_64 -d -p 56733:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+#No mac adicionar --platform linux/x86_64 
+docker build -t ${app} .
+docker run  -d -p 8080:80 \
+  -v $PWD/app:/app ${app}
